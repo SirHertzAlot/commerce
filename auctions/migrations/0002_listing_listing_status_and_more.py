f1d4163a98355c1c9b592b82b33dae0f40b4c35b
SirHertzAlot <dukjoseph@gmail.com>
2024-07-04 05:46:47 -0400

    Fixed formatting issues in files for pre-commit hook.

--- auctions/migrations/0002_listing_listing_status_and_more.py
+++ auctions/migrations/0002_listing_listing_status_and_more.py
@@ -2,29 +2,43 @@
 
 import datetime
 import uuid
+
 from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auctions', '0001_initial'),
+        ("auctions", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='listing',
-            name='listing_status',
-            field=models.CharField(choices=[('Active', 'Active'), ('Expired', 'Expired')], default='Active', max_length=10),
+            model_name="listing",
+            name="listing_status",
+            field=models.CharField(
+                choices=[("Active", "Active"), ("Expired", "Expired")],
+                default="Active",
+                max_length=10,
+            ),
         ),
         migrations.AlterField(
-            model_name='listing',
-            name='Listing_startTime',
-            field=models.DateTimeField(default=datetime.datetime(2024, 6, 26, 3, 20, 21, 645832)),
+            model_name="listing",
+            name="Listing_startTime",
+            field=models.DateTimeField(
+                default=datetime.datetime(2024, 6, 26, 3, 20, 21, 645832)
+            ),
         ),
         migrations.AlterField(
-            model_name='listing',
-            name='listing_id',
-            field=models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True),
+            model_name="listing",
+            name="listing_id",
+            field=models.UUIDField(
+                db_index=True,
+                default=uuid.uuid4,
+                editable=False,
+                primary_key=True,
+                serialize=False,
+                unique=True,
+            ),
         ),
     ]
